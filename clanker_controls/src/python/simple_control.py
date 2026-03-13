778946730fc321135f3de9f04abad3dae56da2a5
spyalexs <spyalexs@gmail.com>
2026-03-13 12:50:37 -0600

    actually propagate time

--- clanker_controls/src/python/simple_control.py
+++ clanker_controls/src/python/simple_control.py
@@ -100,6 +100,7 @@ class SimpleController(Node):
 
         anglular_rate = self.angle_p_gain * state_error[1] + self.angle_i_gain * self.integral[1] + self.angle_d_gain * state_error_derivatives[1]
 
+        self.previous_tic_stamp = tic_time
 
         #send out the message
         msg = Twist()
